5fd620ebf5cbec02bd5cd13592548b15677c5319
adelso99 <adelsohernandez99@gmail.com>
2024-12-31 17:08:17 -0600

    vamos x3

---
+++ js/09_while.js
@@ -0,0 +1,20 @@
+'use strict'
+// bucle WHILE: while significa mientras
+// Iterador de incrmento ++
+// Iterador de decrmento --
+var year = 2024;
+
+while(year !=2000){
+    //Ejecuta esto
+    console.log("Estamos en el año: "+year);
+    year--;
+}
+
+
+
+// DO WHILE
+var years = 30;
+do{
+    alert("Solo cuando sea diferente a 20");
+    years--;
+}while(years > 25);
