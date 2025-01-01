cdd4aba2ce5a7dc721fb1ef6040664710ecda3ee
adelso99 <adelsohernandez99@gmail.com>
2025-01-01 14:36:33 -0600

    vamos x5

--- js/11_ejercicio.js
+++ js/11_ejercicio.js
@@ -3,13 +3,21 @@
 PROGRAMA QUE PIDA 2 NUMEROS
 y que nos diga cual es el mayor y cual es el
 menor y si son iguales
+PLUS: que si no los numeros no son numeros
+o que son iguales a cero, que lo vuelva a pedir
+los numeros
 */
 
 var numero1 = parseInt(prompt("Introduce el Primer numero", 0));
 var numero2 = parseInt(prompt("Introduce el Segundo numero", 0));
 
-console.log(numero1);
-console.log(numero2);
+// isNaN: esta es una funcion 
+// si no fuera un numero devuelve
+while(numero1 <= 0 || numero2 <= 0 || isNaN(numero1) || isNaN(numero2)){
+         numero1 = parseInt(prompt("Introduce el Primer numero", 0));
+         numero2 = parseInt(prompt("Introduce el Segundo numero", 0));
+
+}
 
 if(numero1 == numero2){
     alert("Numeros iguales");
