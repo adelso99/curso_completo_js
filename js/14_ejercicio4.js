4019147038060d8580320b4288c3f7688ba5035f
adelso99 <adelsohernandez99@gmail.com>
2025-01-08 18:27:11 -0600

    vamos x9

---
+++ js/14_ejercicio4.js
@@ -0,0 +1,28 @@
+'use strict'
+/*
+Mostrar todos los numeros
+impares que hay entre dos numeros indroducidos por
+el usuario
+*/
+
+var numero1 = parseInt(prompt("Introduce el Primer nuemro", 0));
+var numero2 = parseInt(prompt("Introduce el Segundo nuemro", 0));
+
+document.write("<h1>Los Numeros Impares de "+numero1+"hasta "+numero2+"son los siguientes: </h1>")
+
+/*
+for (var i = numero1; i <= numero2; i++) {
+    // Verificar si el número es impar
+    if (i % 2 !== 0) {
+        document.write(i + "<br/>");
+    }
+}
+    */
+
+
+while(numero1 < numero2){
+    numero1++
+    if(numero1%2 != 0){
+        document.write("El "+numero1+"Es Impar");
+    }
+}
