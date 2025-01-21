87d9a8fc84f567391aa72f4e13d17c31d99e5baa
adelso99 <adelsohernandez99@gmail.com>
2025-01-20 22:29:36 -0600

    vamos x17

---
+++ js/19_funciones.js
@@ -0,0 +1,22 @@
+'use strict'
+
+// Funciones
+/*
+Conjunto de ordenes que se ejecutan cuando realizamos 
+la funcion
+Se cree con la palabra recervada. function
+*/
+
+//Definicion de la funcion
+function calculadora(){
+
+    //Conjunto de Instrucciones a ejecutar
+    console.log("Hola soy la calculadora");
+    console.log("Si soy yo");
+
+    return "Hola soy la calculadora!!";
+}
+
+// Invocar o llamar a la funcion
+var resultado = calculadora();
+console.log(resultado);
