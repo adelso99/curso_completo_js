84db0cad49db1bad192afc9747269e936a78b666
adelso99 <adelsohernandez99@gmail.com>
2025-01-24 13:14:08 -0600

    vamos x20

--- js/19_funciones.js
+++ js/19_funciones.js
@@ -8,15 +8,28 @@ Se cree con la palabra recervada. function
 */
 
 //Definicion de la funcion
-function calculadora(){
+function calculadora(numero1, numero2){
 
     //Conjunto de Instrucciones a ejecutar
-    console.log("Hola soy la calculadora");
-    console.log("Si soy yo");
+    console.log("Suma: "+(numero1+numero2));
+    console.log("Resta: "+(numero1-numero2));
+    console.log("Multiplicacion: "+(numero1*numero2));
+    console.log("Division: "+(numero1/numero2));
+    console.log("************************");
+   
+   
+    // console.log("Si soy yo");
 
-    return "Hola soy la calculadora!!";
+    //return "Hola soy la calculadora!!";
 }
 
 // Invocar o llamar a la funcion
-var resultado = calculadora();
-console.log(resultado);
+//var resultado = calculadora();
+//console.log(resultado);
+calculadora(12, 8);
+calculadora(98, 2);
+
+for(var i = 1; i <= 10; i++){
+    console.log(i);
+    calculadora(i, 8);
+}
