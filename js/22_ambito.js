294e1f0d6ade44342292f6ce7087307cae2fd016
adelso99 <adelsohernandez99@gmail.com>
2025-02-04 13:01:29 -0600

    vamos x23

---
+++ js/22_ambito.js
@@ -0,0 +1,15 @@
+'use strict'
+
+
+
+function holaMundo(texto){
+    var hola_Mundo = "Texto dentro de funcion";
+    console.log(texto);
+    console.log(numero.toString());
+    console.log(hola_Mundo);
+}
+
+var numero = 12;
+var texto = "Soy una variable Global";
+holaMundo(texto);
+
