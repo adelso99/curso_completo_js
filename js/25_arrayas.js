ceeaad3989790698882a837823667a54f8083c73
adelso99 <adelsohernandez99@gmail.com>
2025-02-05 11:50:51 -0600

    vamos x27

--- js/25_arrayas.js
+++ js/25_arrayas.js
@@ -6,7 +6,30 @@
 var nombre = "Adelso";
 var nombres = ["Adelso", "Adel", "Kick", "Budotski", 52, true];
 
-var lenguajes = new Array ("PHP", "JS", "Go", "Java");
+var lenguajes = new Array ("PHP", "JS", "Go", "Java", "Pyton", "C++");
 
-console.log(nombres[1]);
+
+/*
+var elemento = parseInt(prompt("Que elemento del Array quieres", 0));
+if(elemento >= nombres.length){
+    alert("Introduce el Numero correcto menor que: " +nombres.length);
+}else{
+    alert("El usuario selccionado es: "+nombres[elemento]);
+}
+*/
+
+//console.log(nombres.length);
 //console.log(lenguajes);
+
+document.write("<h1>Lenguajes de Programacion de 2025</h1>")
+document.write("<ul>");
+for(var i = 0; i < lenguajes.length; i++){
+    document.write("<li>"+lenguajes[i]+"</li>");
+}
+    document.write("</ul>");
+
+
+
+
+//document.write(lenguajes[1]);
+//document.write(lenguajes[2]);
