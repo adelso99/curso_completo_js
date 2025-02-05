397bf96f00208530e7d007f31edde6a6e2471681
adelso99 <adelsohernandez99@gmail.com>
2025-02-04 18:24:03 -0600

    vamos x26

---
+++ js/25_arrayas.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//Arrays, Areglos, Matrices
+// Es una coleccion de varios datos o valores
+
+var nombre = "Adelso";
+var nombres = ["Adelso", "Adel", "Kick", "Budotski", 52, true];
+
+var lenguajes = new Array ("PHP", "JS", "Go", "Java");
+
+console.log(nombres[1]);
+//console.log(lenguajes);
