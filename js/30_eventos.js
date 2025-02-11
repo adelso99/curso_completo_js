28bf972e076d27eff2b8654479d109d23adad3d3
adelso99 <adelsohernandez99@gmail.com>
2025-02-11 11:10:42 -0600

    vamos x44

--- js/30_eventos.js
+++ js/30_eventos.js
@@ -1,73 +1,76 @@
 'use strict'
+//Evento load: para que no exita conflicto en el llamdo del script de html
+window.addEventListener('load', () => {
 
-// Eventos del Raton
+            // Eventos del Raton
+        function cambiarColor(){
+            console.log("Me Has dado Click");
 
+            var bg = boton.style.background;
 
-function cambiarColor(){
-    console.log("Me Has dado Click");
+            if(bg == "green"){
+                boton.style.background = "red";
+                
+            }else{
+                boton.style.background = "green";
+            }
 
-    var bg = boton.style.background;
+            boton.style.padding = "10px";
+            boton.style.border = "1px solid #ccc";
 
-    if(bg == "green"){
-        boton.style.background = "red";
-        
-    }else{
-        boton.style.background = "green";
-    }
+            return true;
+        }
 
-    boton.style.padding = "10px";
-    boton.style.border = "1px solid #ccc";
-
-    return true;
-}
-
-var boton = document.querySelector("#boton");
-
-
-//Click
-boton.addEventListener('click', function(){
-    cambiarColor();
-});
-
-//Mouse Over: pasar por encima de un boton cambia de color
-boton.addEventListener('mouseover', function(){
-    boton.style.background = ("yellow");
-});
-
-//Mouse Out: Similar al Mouse Over, pero cuando sales
-boton.addEventListener('mouseout', function(){
-    boton.style.background = ("#ccc");
-});
+        var boton = document.querySelector("#boton");
 
 
-// Evento Focus
-var input = document.querySelector("#campo_nombre");
+        //Click
+        boton.addEventListener('click', function(){
+            cambiarColor();
+        });
 
-input.addEventListener('focus', function(){
-    console.log("[focus]Estas dentro del Input");
-});
+        //Mouse Over: pasar por encima de un boton cambia de color
+        boton.addEventListener('mouseover', function(){
+            boton.style.background = ("yellow");
+        });
+
+        //Mouse Out: Similar al Mouse Over, pero cuando sales
+        boton.addEventListener('mouseout', function(){
+            boton.style.background = ("#ccc");
+        });
 
 
-// Evento Blur
-input.addEventListener('blur', function(){
-    console.log("[blur]Estas Fuera del Input");
-});
+        // Evento Focus
+        var input = document.querySelector("#campo_nombre");
+
+        input.addEventListener('focus', function(){
+            console.log("[focus]Estas dentro del Input");
+        });
 
 
-// Evento Keydown
-input.addEventListener('keydown', function(event){
-    console.log("[keydown]Estas Pulsando una Tecla", String.fromCharCode(event.keyCode));
-    
-});
+        // Evento Blur
+        input.addEventListener('blur', function(){
+            console.log("[blur]Estas Fuera del Input");
+        });
 
-// Evento Keypress
-input.addEventListener('keypress', function(event){
-    console.log("[keypress]Tecla Precionada", String.fromCharCode(event.keyCode));
-    
-});
 
-// Evento Keyup
-input.addEventListener('keyup', function(event){
-    console.log("[keyup]Tecla Soltada", String.fromCharCode(event.keyCode));
-    
-});
+        // Evento Keydown
+        input.addEventListener('keydown', function(event){
+            console.log("[keydown]Estas Pulsando una Tecla", String.fromCharCode(event.keyCode));
+            
+        });
+
+        // Evento Keypress
+        input.addEventListener('keypress', function(event){
+            console.log("[keypress]Tecla Precionada", String.fromCharCode(event.keyCode));
+            
+        });
+
+        // Evento Keyup
+        input.addEventListener('keyup', function(event){
+            console.log("[keyup]Tecla Soltada", String.fromCharCode(event.keyCode));
+            
+        });
+
+});// Final del evento load
+
