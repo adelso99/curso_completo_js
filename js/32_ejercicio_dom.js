5d745f2c20ca3d409c4ce529e69a1a0cfbb7ffe4
adelso99 <adelsohernandez99@gmail.com>
2025-02-19 18:00:51 -0600

    vamos x47

--- js/32_ejercicio_dom.js
+++ js/32_ejercicio_dom.js
@@ -1,8 +1,13 @@
 'use strict'
 window.addEventListener('load', function(){
+    console.log("DOM, Cargado")
 
+    //2.Boton de enviar el formulario: eventos submit
+    var formulario = document.querySelector("#formulario");
 
+    formulario.addEventListener('submit', function(){
 
+    });
 
 
 
