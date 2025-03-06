0978e480df5379ab020295ab875c00fceab6daa8
adelso99 <adelsohernandez99@gmail.com>
2025-03-06 11:37:39 -0600

    vamos x58

---
+++ js/33_json.js
@@ -0,0 +1,27 @@
+'use strict'
+
+// JSON: JavaScript Object Notation
+// un objeto de define con llavaes {}
+
+var pelicula = {
+    titulo: 'Btman vs Superman',
+    year: 2017,
+    pais: 'Estados Unidos'
+};
+
+// ARAY DE PELICULAS
+var peliculas = [
+    {titulo: "La verdad duele", year: 2016, pais: "Francia"},
+    pelicula
+];
+
+//pelicula.titulo = "Superman Begnis";
+//console.log(pelicula);
+
+var caja_peliculas = document.querySelector("#peliculas")
+var index;
+for(index in peliculas){
+    var p = document.createElement("p");
+    p.append(peliculas[index].titulo+" - "+peliculas[index].year);
+    caja_peliculas.append(p);
+};
