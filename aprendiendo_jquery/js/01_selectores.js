0738e2593ae7220a6ec7621370a58aaba6658df2
adelso99 <adelsohernandez99@gmail.com>
2025-04-03 23:57:52 -0600

    vamos x70

---
+++ aprendiendo_jquery/js/01_selectores.js
@@ -0,0 +1,20 @@
+'use strict'
+// $ o jQuery: hace referencia a jQuery
+//Selector: es lo siguiente despues del signo $ o jQuery
+
+
+$(document).ready(function(){
+    //Selectores de ID
+    $("#rojo").css("background","red")
+                .css("color", "white");
+
+    $("#amarillo").css("background", "yellow")
+                  .css("color", "green");
+
+    $("#verde").css("background", "green")
+                  .css("color", "white");
+    
+
+
+
+});
