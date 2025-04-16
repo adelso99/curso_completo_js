adc2577c88e7bc4ea9fd1993b7525ded867822d7
adelso99 <adelsohernandez99@gmail.com>
2025-04-15 19:31:57 -0600

    vamos x88

--- aprendiendo_jquery/js/02_eventos.js
+++ aprendiendo_jquery/js/02_eventos.js
@@ -42,6 +42,7 @@ $(document).ready(function(){
 
      //Eventos Focus y Blur 
      var nombre = $("#nombre");
+     var datos = $("#datos");
 
      nombre.focus(function(){
          $(this).css("border", "2px solid green");
@@ -50,8 +51,31 @@ $(document).ready(function(){
      nombre.blur(function(){
       $(this).css("border", "1px solid #ccc");
 
-      $("#datos").text($(this).val()).show();
+      datos.text($(this).val()).show();
   });
 
+
+  //Eventos de Mousedown y Mouseup
+  // var datos = $("#datos"); esta agregada la variable arriba
+  // pero le dare mas uso en este apartado  
+   datos.mousedown(function(){
+      $(this).css("border-color", "gray");
+   });
+
+   datos.mouseup(function(){
+      $(this).css("border-color", "black");
+   });
   
+
+   //Evneto Mousemove
+   $(document).mousemove(function(){
+      //console.log("en X: "+event.clientX);
+      //console.log("en Y: "+event.clientY);
+
+      //Ocultar el curso del mouse con la siguiente linea
+      $('body').css("cursor","none");
+
+     $("#sigueme").css("left",event.clientX)      
+                  .css("top",event.clientY);      
+   });
 });
