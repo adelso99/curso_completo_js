ca9d304a5d119b96fea2e2e090b6a3b3001f2a7b
adelso99 <adelsohernandez99@gmail.com>
2025-04-17 00:22:17 -0600

    vamos x78

--- aprendiendo_jquery/js/05_ajax.js
+++ aprendiendo_jquery/js/05_ajax.js
@@ -3,6 +3,24 @@ $(document).ready(function(){
    // alert("Ajax Hola");
 
    //1. Metodo Load
-   $("#datos").load("https://reqres.in/");
+   //$("#datos").load("https://reqres.in/");
+
+   //Metodos GET y POST
+   //GET
+   $.get("https://reqres.in/api/users", {page: 2}, function(response){
+    response.data.forEach((element, index) => {
+        $("#datos").append("<p>"+element.first_name+" "+element.last_name+"</p>");
+    });
+   });
+    
+    //Ejemplo 2 de GET
+   //$.get("https://reqres.in/api/users", { page: 1 }, function(response){
+    // Recorrer cada usuario y agregarlo al div
+    //response.data.forEach(function(usuario){
+      //$('#datos').append(
+       // "<p>" + usuario.first_name + " " + usuario.last_name + "</p>"
+    //  );
+   // });
+ // });
 
 });
