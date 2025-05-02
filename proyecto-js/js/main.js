4b639e2805765cd7ed62cf2bc918ad2ef7167d78
adelso99 <adelsohernandez99@gmail.com>
2025-05-02 01:03:49 -0600

    vamos x108

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -81,4 +81,16 @@ $(document).ready(function(){
         theme.attr("href", "css/purple.css");
     });
 
+    //Scroll arriba de la web
+    $('.subir').click(function(e){
+      e.preventDefault();
+
+        $('html, body').animate({
+           scrollTop: 0
+        }, 500);
+
+        return false;
+
+    });
+
 });
