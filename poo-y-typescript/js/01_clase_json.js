49d51dcff4e8697e0553b4b032b5a1e465608c52
adelso99 <adelsohernandez99@gmail.com>
2025-05-09 12:23:44 -0600

    vamos x123

---
+++ poo-y-typescript/js/01_clase_json.js
@@ -0,0 +1,19 @@
+
+//Obejto JSON
+var bicicleta = {
+    color: 'rojo',
+    modelo: 'bmx',
+    freno: 'disco',
+    velocidadMaxima: '60km',
+    cambiarColor: function(nuevo_color){
+       // bicicleta.color = nuevo_color: se puede asi o
+       this.color = nuevo_color;
+       console.log(this);
+
+    }
+
+};
+
+bicicleta.cambiarColor("Azul");
+
+
