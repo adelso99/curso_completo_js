9ba7a6b459d4a1c8810342858c86a4b66a3e70a3
adelso99 <adelsohernandez99@gmail.com>
2025-05-15 23:12:58 -0600

    vamos x131

--- poo-y-typescript/js/02_holamundo.ts
+++ poo-y-typescript/js/02_holamundo.ts
@@ -1,3 +1,3 @@
 console.log("Hola mundo con TypeScript");
-alert("Hola Mundo  con TS con Adelso Hernandez");                                                                            
+alert("Hola Mundo  con TS con Adelso Hernandez en la Web");                                                                            
 
