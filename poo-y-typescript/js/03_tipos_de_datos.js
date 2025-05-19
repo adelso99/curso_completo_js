34a6214d5146268f8e6f04a0c0da55617fa1a214
adelso99 <adelsohernandez99@gmail.com>
2025-05-18 23:42:23 -0600

    vamos x141

--- poo-y-typescript/js/03_tipos_de_datos.js
+++ poo-y-typescript/js/03_tipos_de_datos.js
@@ -13,11 +13,15 @@ cualquiera = 72;
 var lenguajes = ["PHP", "JS", "CSS"];
 // Forma 2
 var years = [2, 3, 21, 12];
+//Let vs var
+// var funciona a nivel global
+var numero1 = 10;
+var numero2 = 12;
+if (numero1 == 10) {
+    // let funciona a nivel de bloque
+    var numero1_1 = 44;
+    var numero2_1 = 55;
+    console.log(numero1_1, numero2_1);
+}
+console.log(numero1, numero2);
 console.log(cadena, numero, verdadero_falso, cualquiera, lenguajes, years);
-
-/*
-pogress lest go..........1
-*/
-/*
-pogress lest go..........2
-*/
--- poo-y-typescript/js/03_tipos_de_datos.ts
+++ poo-y-typescript/js/03_tipos_de_datos.ts
@@ -25,4 +25,18 @@ var lenguajes: Array<string> = ["PHP", "JS", "CSS"];
 // Forma 2
 let years: number[] = [2, 3, 21, 12];
 
+//Let vs var
+// var funciona a nivel global
+var numero1 = 10;
+var numero2 = 12;
+
+if(numero1 == 10){
+    // let funciona a nivel de bloque
+    let numero1 = 44;
+    let numero2 = 55;
+
+    console.log(numero1, numero2);
+}
+    console.log(numero1, numero2);
+
 console.log(cadena, numero, verdadero_falso, cualquiera, lenguajes, years);
