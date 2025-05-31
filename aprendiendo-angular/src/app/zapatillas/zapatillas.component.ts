13b57dcce441359336bfb9f6d1f4fb41f6172407
adelso99 <adelsohernandez99@gmail.com>
2025-05-30 23:50:56 -0600

    vamos x220

--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -1,11 +1,25 @@
 //Deifincion de la Importacion del modulo del paquete
-import { Component } from "@angular/core";
+import { Component, OnInit} from "@angular/core";
+// Importacion de la clase Zapatilla.ts 
+// .. Porque estoy en otra carpeta
+import { Zapatilla } from "../models/zapatilla";
 
 //Definicion de metadatos del componente
 @Component({
     selector: 'zapatillas',
     templateUrl: './zapatillas.component.html'
 })
-export class ZapatillasComponent {
+export class ZapatillasComponent implements OnInit{
     public titulo: string = "Componente de Zapatillas";
+    public zapatillas: Array<Zapatilla>;
+
+    constructor(){
+        this.zapatillas = [
+            new Zapatilla('Reebook Classic', 'Reebook', 'Blanco', 80, true)
+        ];
+    }
+
+    ngOnInit(){
+        console.log(this.zapatillas);        
+    }
 }
