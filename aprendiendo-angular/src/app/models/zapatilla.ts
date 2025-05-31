3de7d1e6c29422584efc62b7552f6dfd67cbd9ce
adelso99 <adelsohernandez99@gmail.com>
2025-05-30 23:44:36 -0600

    vamos x219

--- aprendiendo-angular/src/app/models/zapatilla.ts
+++ aprendiendo-angular/src/app/models/zapatilla.ts
@@ -1,7 +1,35 @@
 export class Zapatilla{
-    public nombre: string;
-    public marca: string;
-    public color: string;
-    public precio: number;
-    public stock: boolean;
+   
+   //Para no hacer todo esto que renudundar el codigo
+    //se puede hacer de esta manera
+            /*
+        export class Zapatilla{
+            public nombre: string;
+            public marca: string;
+            public color: string;
+            public precio: number;
+            public stock: boolean;
+
+    
+            constructor(nombre, marca, color, precio, stock){
+                this.nombre = nombre;
+                this.marca = marca;
+                this.color = color;
+                this.precio = precio;
+                this.stock = stock;
+            }
+            */    
+    // de esta manera solo que se cambia
+    // esta definido dentro del parentesis
+    // el export arriba por etetica
+    constructor(
+        public nombre: string,
+        public marca: string,
+        public color: string,
+        public precio: number,
+        public stock: boolean
+
+    ){}
+
+
 }
