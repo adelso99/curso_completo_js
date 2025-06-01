e567d82e0c4226006636774bc4af5efcf18e4915
adelso99 <adelsohernandez99@gmail.com>
2025-05-31 23:57:08 -0600

    vamos x228

--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -12,24 +12,34 @@ import { Zapatilla } from "../models/zapatilla";
 export class ZapatillasComponent implements OnInit{
     public titulo: string = "Componente de Zapatillas";
     public zapatillas: Array<Zapatilla>;
-    public marcas: Array[string];
+    public marcas: String[];
 
     constructor(){
+        this.marcas = new Array();
         this.zapatillas = [
             new Zapatilla('Vans News', 'Vans', 'Rojas', 90, true),
             new Zapatilla('Reebook Classic', 'Reebook', 'Blanco', 80, true),
+            new Zapatilla('Reebook new', 'Reebook', 'red', 810, true),
             new Zapatilla('Nike Panda', 'Nike', 'Negras', 60, true),
             new Zapatilla('Nike AF1', 'Nike', 'Blanco', 150, false)
         ];
     }
 
     ngOnInit(){
-        console.log(this.zapatillas);        
+        console.log(this.zapatillas); 
+        
+        this.getMarcas();
     }
 
-    marcas(){
-        this.zapatillas.forEach((value, index) =>{
-
+    getMarcas(){
+        this.zapatillas.forEach((zapatilla, index) =>{
+           
+            if(this.marcas.indexOf(zapatilla.marca) < 0){
+                this.marcas.push(zapatilla.marca);
+            }
+              
         });
+        console.log(this.marcas);
+
     }
 }
