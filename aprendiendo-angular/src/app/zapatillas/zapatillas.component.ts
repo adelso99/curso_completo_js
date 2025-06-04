a1b6cf11e0ee00d8cd1b06a106d13ce74420ae7a
adelso99 <adelsohernandez99@gmail.com>
2025-06-03 22:50:47 -0600

    vamos x257

--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -13,8 +13,10 @@ export class ZapatillasComponent implements OnInit{
     public titulo: string = "Componente de Zapatillas";
     public zapatillas: Array<Zapatilla>;
     public marcas: String[];
+    public color: string;
 
     constructor(){
+        this.color = 'blue';
         this.marcas = new Array();
         this.zapatillas = [
             new Zapatilla('Vans News', 'Vans', 'Rojas', 90, true),
