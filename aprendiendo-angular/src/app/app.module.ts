915e66105df7f5480157640fc3b40e3231364227
adelso99 <adelsohernandez99@gmail.com>
2025-06-13 23:11:32 -0600

    vamos x331

--- aprendiendo-angular/src/app/app.module.ts
+++ aprendiendo-angular/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { VideojuegoComponent } from './videojuego/videojuego.component';
 import { ZapatillasComponent } from './zapatillas/zapatillas.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     VideojuegoComponent,
     ZapatillasComponent,
-    CursosComponent
+    CursosComponent,
+    HomeComponent
     
   ],
   imports: [
--- aprendiendo-angular/src/app/app.routing.ts
+++ aprendiendo-angular/src/app/app.routing.ts
@@ -4,3 +4,21 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 //Importacion de los componentes
+import {  HomeComponent } from './home/home.component';
+import { ZapatillasComponent } from './zapatillas/zapatillas.component';
+import { VideojuegoComponent } from './videojuego/videojuego.component';
+import { CursosComponent } from './cursos/cursos.component';
+
+//Arrays de las rutas 
+const appRoutes: Routes = [
+    {path: '', component:  HomeComponent},
+    {path: 'zapatillas', component:  ZapatillasComponent},
+    {path: 'videojuego', component:  VideojuegoComponent},
+    {path: 'cursos', component:  CursosComponent},
+    {path: **, component: HomeComponent}
+
+];
+
+//Exportar el modulo del router
+export const appRoutingProviders: any[] = [];
+export const Routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
