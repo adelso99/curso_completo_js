6df6d21d4d77c78cb15fa60419e4fdfb3bc0c2d2
adelso99 <adelsohernandez99@gmail.com>
2025-06-13 23:00:03 -0600

    vamos x330

---
+++ aprendiendo-angular/src/app/app.routing.ts
@@ -0,0 +1,6 @@
+//importar los modulos de router de angular
+
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+//Importacion de los componentes
