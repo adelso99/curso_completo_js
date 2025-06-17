c466123c004059bbf73c0c2a7807c2e59e028281
adelso99 <adelsohernandez99@gmail.com>
2025-06-16 21:40:57 -0600

    vamos x377

--- aprendiendo-angular/src/app/app.routing.ts
+++ aprendiendo-angular/src/app/app.routing.ts
@@ -8,15 +8,17 @@ import { HomeComponent } from './home/home.component';
 import { ZapatillasComponent } from './zapatillas/zapatillas.component';
 import { VideojuegoComponent } from './videojuego/videojuego.component';
 import { CursosComponent } from './cursos/cursos.component';
+import { ExternoComponent } from './externo/externo.component';
 
 //Arrays de las rutas 
 const appRoutes: Routes = [
-    {path: '', component:  HomeComponent},
-    {path: 'home', component:  HomeComponent},
-    {path: 'zapatillas', component:  ZapatillasComponent},
-    {path: 'videojuego', component:  VideojuegoComponent},
-    {path: 'cursos', component:  CursosComponent},
-    {path: 'cursos/:nombre/:followers', component:  CursosComponent},
+    {path: '', component: HomeComponent},
+    {path: 'home', component: HomeComponent},
+    {path: 'zapatillas', component: ZapatillasComponent},
+    {path: 'videojuego', component: VideojuegoComponent},
+    {path: 'cursos', component: CursosComponent},
+    {path: 'cursos/:nombre/:followers', component: CursosComponent},
+    {path: 'externo', component: ExternoComponent},
     {path: '**', component: HomeComponent}
 
 ];
