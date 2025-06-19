ce333ea205d483a49adf40a2ee722ad14929cd4c
adelso99 <adelsohernandez99@gmail.com>
2025-06-19 12:17:22 -0600

    vamos x373

--- aprendiendo-angular/src/app/service/peticiones.service.ts
+++ aprendiendo-angular/src/app/service/peticiones.service.ts
@@ -20,4 +20,12 @@ export class PeticionesService {
     return this._http.get(this.url+'api/users/'+userId, { headers });
   }
 
+  addUser(user): Observable<any>{
+    let params = JSON.stringify(user);
+    let headers = new HttpHeaders().set('Content-Type', 'aplication/json');
+
+    return this._http.post(this.url+'api/users', params, {headers: headers});
+
+  }
+
 }
