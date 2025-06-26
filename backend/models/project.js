3f5ed897920f4bfad11b74b009a584c90cab3e0a
adelso99 <adelsohernandez99@gmail.com>
2025-06-26 16:17:59 -0600

    vamos x420

---
+++ backend/models/project.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ProjectSchema = Schema({
+    name: String,
+    description: String,
+    category: String,
+    year: Number,
+    langs: [String]
+});
+
+//Exportacion
+module.exports = mongoose.model('Project', ProjectSchema);
+//Projects --> guarda los documentos en la coleccion 
