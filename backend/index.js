1d4a6dc62f032db45b59141add9023662afb8fb1
adelso99 <adelsohernandez99@gmail.com>
2025-06-25 22:52:01 -0600

    vamos x405

--- backend/index.js
+++ backend/index.js
@@ -8,3 +8,7 @@ mongoose.connect('mongodb://localhost:27017/portafolio')
         console.log("Conexion a la Base de Datos Exitosa.....");
     })
     .catch(err => console.log(err));
+
+/*
+Gooo
+*/
