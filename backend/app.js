3c6c96fbf8f595d95e63e15d724821e591fbef0c
adelso99 <adelsohernandez99@gmail.com>
2025-06-26 12:40:27 -0600

    vamos x418

---
+++ backend/app.js
@@ -0,0 +1,34 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+//Cargar Archivos de Rutas
+
+//Configuracion de middlewares
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//Configuracion de CORS
+
+
+//Configuracion de Rutas
+
+    //ruta de prueba 1
+app.get('/test', (req, res) => {
+    res.status(200).send({
+        message: "Hola Mundo desde la API nodejs"
+    });
+});
+
+    //ruta de prueba 2
+    app.get('/', (req, res) => {
+        res.status(200).send(
+            "<h1>Esta es la Pagina de Incio</h1>"
+        );
+    });
+
+//Exportacion de archivos
+module.exports = app;
