4c4440533b5c193eccbdf0a2730a48e8ee12f615
adelso99 <adelsohernandez99@gmail.com>
2025-06-26 22:42:25 -0600

    vamos x421

--- backend/app.js
+++ backend/app.js
@@ -16,23 +16,7 @@ app.use(bodyParser.json());
 
 //Configuracion de Rutas
 
-    //ruta de prueba 1
-app.post('/test/:id', (req, res) => {
-    console.log(req.body.nombre);
-    console.log(req.query.web);
-    console.log(req.params.id);
 
-    res.status(200).send({
-        message: "Hola Mundo desde la API nodejs"
-    });
-});
-
-    //ruta de prueba 2
-    app.get('/', (req, res) => {
-        res.status(200).send(
-            "<h1>Esta es la Pagina de Incio</h1>"
-        );
-    });
 
 //Exportacion de archivos
 module.exports = app;
---
+++ backend/controllers/project.js
@@ -0,0 +1,13 @@
+'use strict'
+
+var controller = {
+    home: function(){
+
+    },
+
+    test: function(){
+
+    }
+};
+
+module.exports = controller;
