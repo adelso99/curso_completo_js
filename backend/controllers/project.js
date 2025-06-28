000221ed65155ccabbfabcae439b004cca8375c6
adelso99 <adelsohernandez99@gmail.com>
2025-06-27 20:16:32 -0600

    vamos x423

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -17,11 +17,23 @@ var controller = {
     
     saveProject: function(req, res){
         var project = new Project();
-        
-        return res.status(200).send({
-               message: "Metodo saveProject"
-        });
 
+        var params = req.body;
+        project.name = params.name;
+        project.description = params.description;
+        project.category = params.category;
+        project.year = params.year;
+        project.langs = params.langs;
+        project.image = null;
+
+        project.save((err, projectStored) => {
+            if(err) return res.status(500).send({message: 'Error al guardar documento'})
+            
+            if(!projectStored) return res.status(404).send({message: 'No se a podido guardar el proyecto'});
+
+            return res.status(200).send({project: projectStored});
+        });
+        
     }
 };
 
