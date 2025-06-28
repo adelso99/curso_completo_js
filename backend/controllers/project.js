d42d97ef7c497313cfbbaa75abde40f4abe03fa1
adelso99 <adelsohernandez99@gmail.com>
2025-06-27 19:51:00 -0600

    vamos x422

--- backend/controllers/project.js
+++ backend/controllers/project.js
@@ -1,5 +1,7 @@
 'use strict'
 
+var Project = require('../models/project');
+
 var controller = {
     home: function(req, res){
         return res.status(200).send({
@@ -11,6 +13,15 @@ var controller = {
         return res.status(200).send({
             message: "Soy el metodo o la accion de test del controlador de project" 
          });
+    },
+    
+    saveProject: function(req, res){
+        var project = new Project();
+        
+        return res.status(200).send({
+               message: "Metodo saveProject"
+        });
+
     }
 };
 
