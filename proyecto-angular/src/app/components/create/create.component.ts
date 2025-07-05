dd2df902544d749ae2c5bba35b8178b41ee376b9
adelso99 <adelsohernandez99@gmail.com>
2025-07-04 23:49:10 -0600

    vamos x447

--- proyecto-angular/src/app/components/create/create.component.ts
+++ proyecto-angular/src/app/components/create/create.component.ts
@@ -1,13 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../../models/project';
+import { ProjectService } from 'src/app/services/project.service';
+
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.css'],
+  providers: [ProjectService]
 })
 export class CreateComponent implements OnInit {
 
-  constructor() { }
+  public title: string;
+  public project: Project; 
+
+  constructor(
+      private _projectService: ProjectService
+  ){
+      this.title = "Crear Proyecto";
+      this.project = new Project('','','','',2025,'','');
+   }
 
   ngOnInit() {
   }
