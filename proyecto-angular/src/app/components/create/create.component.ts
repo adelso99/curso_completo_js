9d14da789e7216918871e03c5bbf2c7ccd05f6d6
adelso99 <adelsohernandez99@gmail.com>
2025-07-08 23:45:13 -0600

    vamos x467

--- proyecto-angular/src/app/components/create/create.component.ts
+++ proyecto-angular/src/app/components/create/create.component.ts
@@ -13,6 +13,7 @@ export class CreateComponent implements OnInit {
 
   public title: string;
   public project: Project; 
+  public status: string;
 
   constructor(
       private _projectService: ProjectService
@@ -28,7 +29,10 @@ export class CreateComponent implements OnInit {
     console.log(this.project);
     this._projectService.saveProject(this.project).subscribe(
       response => {
-          console.log(response);
+          if(response.project){
+              this.status = 'success';
+          }else{}
+              this.status = 'failed';
       }, 
       error => {
         console.log(<any>error);
