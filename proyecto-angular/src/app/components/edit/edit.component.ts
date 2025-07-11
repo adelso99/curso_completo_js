507c595fbe033736a4e2f4a46496599a3b97986f
adelso99 <adelsohernandez99@gmail.com>
2025-07-10 23:00:24 -0600

    vamos x496

--- proyecto-angular/src/app/components/edit/edit.component.ts
+++ proyecto-angular/src/app/components/edit/edit.component.ts
@@ -1,15 +1,83 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/models/project';
+import { ProjectService } from 'src/app/services/project.service';
+import { UploadService } from 'src/app/services/upload.service';
+import { Global } from 'src/app/services/global';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-edit',
-  templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.css']
+  templateUrl: '../create/create.component.html',
+  styleUrls: ['./edit.component.css'],
+  providers: [ProjectService, UploadService]
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
+  public title: string;
+  public project: Project;
+  public save_project; 
+  public status: string;
+  public filesToUpload: Array<File>;
+  public url: string;
 
-  ngOnInit() {
+  constructor(
+      private _projectService: ProjectService,
+      private _uploadService: UploadService,
+      private _route: ActivatedRoute,
+      private _router: Router
+  ){
+      this.title = "Editar Proyecto";
+      this.url = Global.url;
+   }
+
+   ngOnInit() {
+    this._route.params.subscribe(params => {
+        let id = params.id;
+
+        this.getProject(id);
+    });
+  }
+
+  getProject(id){
+    this._projectService.getProject(id).subscribe(
+      response => {
+        this.project = response.project;
+      }, 
+      error => {
+        console.log(<any>error);
+      }
+    )
+  }
+
+  onSubmit(){
+    this._projectService.updateProject(this.project).subscribe(
+      response => {
+        if(response.project){
+
+          //Esta parte es para subir la Imagen
+          if(this.filesToUpload){
+                this._uploadService.makeFileRequest(Global.url+"upload-image/"+response.project._id, [], this.filesToUpload, 'image')
+                .then((result:any) => {
+                  this.save_project = result.project;
+                  this.status = 'success';
+                });
+          }else{
+            this.save_project = response.project;
+            this.status = 'success';
+          }
+
+    }else{
+        this.status = 'failed';
+    }  
+      }, 
+      error => {
+        console.log(<any>error);
+      }
+    );
+  }
+
+  fileChangeEvent(fileInput: any){
+    this.filesToUpload = <Array<File>>fileInput.target.files;
   }
 
 }
--- proyecto-angular/src/app/services/project.service.ts
+++ proyecto-angular/src/app/services/project.service.ts
@@ -44,4 +44,12 @@ export class ProjectService{
         return this._http.delete(this.url+'project/'+id, {headers: headers});
     }
 
+    updateProject(project): Observable<any>{
+        let params = JSON.stringify(project);
+        let headers = new HttpHeaders().set('Content-Type', 'application/json');
+        
+        //put: sirve para actualizar un recurso
+        return this._http.put(this.url+'project/'+project._id, params, {headers: headers});
+    }
+
 }
