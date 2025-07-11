a84db96fa6c228372b6cc39b2e2588c5f718c8aa
adelso99 <adelsohernandez99@gmail.com>
2025-07-10 19:40:22 -0600

    vamos x492

--- proyecto-angular/src/app/components/detail/detail.component.ts
+++ proyecto-angular/src/app/components/detail/detail.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Project } from 'src/app/models/project';
 import { ProjectService } from 'src/app/services/project.service';
 import { Global } from 'src/app/services/global';
-import { Router, ActivatedRouter, Params } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
 
 
 @Component({
@@ -13,20 +14,33 @@ import { Router, ActivatedRouter, Params } from '@angular/router';
 })
 export class DetailComponent implements OnInit {
   public url: string;
+  public project: Project;
 
   constructor(
     private _projectService: ProjectService,
     private _router: Router,
-    private _route: ActivatedRouter
+    private _route: ActivatedRoute
   ){
     this.url = Global.url;
   }
 
   ngOnInit() {
+    this._route.params.subscribe(params => {
+        let id = params.id;
+
+        this.getProject(id);
+    });
   }
 
   getProject(id){
-
+    this._projectService.getProject(id).subscribe(
+      response => {
+        this.project = response.project;
+      }, 
+      error => {
+        console.log(<any>error);
+      }
+    )
   }
 
 }
