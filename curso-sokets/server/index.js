d82815d52d0be7a6f2373ab141182c20cc2123c4
adelso99 <adelsohernandez99@gmail.com>
2025-07-22 23:32:29 -0600

    vamos x567

--- curso-sokets/server/index.js
+++ curso-sokets/server/index.js
@@ -3,6 +3,10 @@
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
-
+//creacion del Servidor con exprees
+server.listen(6677, function(){
+    console.log('Servidor esta Funcionando en http://localhost:6677');
+});
 
