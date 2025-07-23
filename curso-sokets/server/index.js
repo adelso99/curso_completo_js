b3a5904204f18ab271467cff03778e526d04c4dd
adelso99 <adelsohernandez99@gmail.com>
2025-07-22 23:37:28 -0600

    vamos x568

--- curso-sokets/server/index.js
+++ curso-sokets/server/index.js
@@ -1,9 +1,13 @@
-
 //carga de paquete con modulo require
 var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+
+app.get('/hola-mundo', function(req, res){
+    res.status(200).send('Hola mundo desde una ruta');
+
+});
 
 //creacion del Servidor con exprees
 server.listen(6677, function(){
