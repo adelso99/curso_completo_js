b9c38dc94c77ef9b3398a212023338310e552a9e
adelso99 <adelsohernandez99@gmail.com>
2025-07-24 23:03:05 -0600

    vamos x579

--- ext-cursojs/carro.ts
+++ ext-cursojs/carro.ts
@@ -1,3 +1,22 @@
-class carro{
-    
+class Carro{
+    public color: string;
+    public modelo: string;
+    public velocidad: number;
+
+    public getColor(){
+        return this.color;
+
+    }
+
+    public setColor(color: string){
+        this.color = color;
+    }
+
 } 
+
+
+var carro = new Carro();
+
+carro.setColor("ROJO");
+
+console.log("El color del Carro es de "+carro.getColor());
