ece46f37a0d5a7834f5a72cb2ea91e9724525c1e
adelso99 <adelsohernandez99@gmail.com>
2025-07-26 00:27:22 -0600

    vamos x590

---
+++ ext-cursojs/programa.ts
@@ -0,0 +1,47 @@
+class Programa{
+    public nombre!: string;
+    public version!: number;
+
+    
+    getNombre(){
+         return this.nombre;
+    }
+
+    setNombre(nombre:string){
+        this.nombre = nombre;
+    }
+
+    getVersion(){
+        return this.version;
+   }
+
+    setVersion(version:number){
+        this.version = version;
+    }
+
+}
+
+class EditorVideo extends Programa{
+    public timeline!:number;
+
+    setTimeline(timeline:number){
+        this.timeline = timeline;
+    }
+
+    getTimeline(){
+        return this.timeline;
+   }
+
+   getAllData():string{
+        return this.getNombre()+" - "+this.getVersion()+" - "+this.getTimeline();
+   }
+}
+
+var editor = new EditorVideo();
+editor.setNombre("Hola estudio");
+editor.setVersion(8);
+editor.setTimeline(4000);
+
+
+console.log(editor.getAllData());
+
