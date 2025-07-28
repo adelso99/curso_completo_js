6a9b54f82a96273be380bf8d019635b76734b628
adelso99 <adelsohernandez99@gmail.com>
2025-07-27 23:39:55 -0600

    vamos x595

--- ext-cursojs/carro.js
+++ ext-cursojs/carro.js
@@ -54,9 +54,16 @@ console.log("El color del Carro es de "+carro.getColor());
 console.log("El color del Carro es de "+carro1.getColor());
 console.log("El color del Carro es de "+carro2.getColor());
 */
+
 /*
 Progrees ....1
 */
 /*
 Progrees ....2
+*/
+/*
+Progrees ....3
+*/
+/*
+Progrees ....4
 */
